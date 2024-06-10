e16a32238e5355aab933ccc756fbeaab54de98d4
vishal-menon <vishaljmenon@gmail.com>
2024-06-10 12:49:42 +0530

    Removed decimal places

--- src/components/PetSlot.jsx
+++ src/components/PetSlot.jsx
@@ -9,7 +9,7 @@ export const PetSlot = (props) => {
                             <img src={props.details.imgPath} alt={props.details.name} className='m-3 rounded-full flex h-36 w-36 bg-yellow-300 text-center' />
                             <div className="p-1 w-40 h-8 bg-slate-700 rounded-2xl">
                                 <div className="w-40 h-6 p-0.5  bg-red-700 rounded-2xl text-center text-sm transition-all" style={props.details.currHp ? { width: `${(props.details.currHp / props.details.hp) * 100}%` } : { width: '0%' }}>
-                                    {`${props.details.currHp.toFixed(2)}/${props.details.hp}`}
+                                    {`${props.details.currHp}/${props.details.hp}`}
                                 </div>
                             </div>
                         </div>
@@ -18,7 +18,7 @@ export const PetSlot = (props) => {
                             <img src={props.details.imgPath} alt={props.details.name} className='m-3 rounded-full flex h-36 w-36 bg-yellow-600 text-center' />
                             <div className="p-1 w-40 h-8 bg-slate-700 rounded-2xl">
                                 <div className="w-40 h-6 p-0.5  bg-red-700 rounded-2xl text-center text-sm transition-all" style={props.details.currHp ? { width: `${(props.details.currHp / props.details.hp) * 100}%` } : { width: '0%' }}>
-                                    {`${props.details.currHp.toFixed(2)}/${props.details.hp}`}
+                                    {`${props.details.currHp}/${props.details.hp}`}
                                 </div>
                             </div>
                         </div>
@@ -29,7 +29,7 @@ export const PetSlot = (props) => {
                         <div className="flex">
                             <div className="w-40 h-8 p-1 bg-slate-700 rounded-2xl">
                                 <div className="w-40 h-6 p-0.5  bg-red-700 rounded-2xl text-center text-sm transition-all" style={props.details.currHp ? { width: `${(props.details.currHp / props.details.hp) * 100}%` } : { width: '0%' }}>
-                                    {`${props.details.currHp.toFixed(2)}/${props.details.hp}`}
+                                    {`${props.details.currHp}/${props.details.hp}`}
                                 </div>
                             </div>
                             <img src={props.details.imgPath} alt={props.details.name} className='m-3 rounded-full flex h-36 w-36 bg-yellow-300 text-center' />
@@ -39,7 +39,7 @@ export const PetSlot = (props) => {
                             <div className="w-40 h-8 p-1 bg-slate-700 rounded-2xl">
                                 <div className="w-40 h-6 p-0.5 bg-red-700 rounded-2xl text-center text-sm transition-all" style={props.details.currHp ?
                                     { width: `${(props.details.currHp / props.details.hp) * 100}%` } : { width: '0%' }}>
-                                    {`${props.details.currHp.toFixed(2)}/${props.details.hp}`}
+                                    {`${props.details.currHp}/${props.details.hp}`}
                                 </div>
                             </div>
                             <img src={props.details.imgPath} alt={props.details.name} className='m-3 rounded-full flex h-36 w-36 bg-yellow-600 text-center' />
