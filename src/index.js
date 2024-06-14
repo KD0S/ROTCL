21b531c972639b90570a2e52db0d0ba91f9e5163
vishal-menon <vishaljmenon@gmail.com>
2024-06-14 18:17:48 +0530

    Testing adsense

--- src/index.js
+++ src/index.js
@@ -4,6 +4,21 @@ import App from './App';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/Authprovider';
 
+// Function to add AdSense script to the head
+function addAdSenseScript() {
+  const script = document.createElement('script');
+  script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7459200310134436";
+  script.async = true;
+  script.crossOrigin = "anonymous";
+  document.head.appendChild(script);
+}
+
+// Conditionally load AdSense script in production environment
+if (process.env.REACT_APP_ENV === 'production') {
+  addAdSenseScript();
+}
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
