5945284679e8f14534cec26e860c77a94a69631f
vishal-menon <vishaljmenon@gmail.com>
2024-06-18 12:21:47 +0530

    Messing around with framer-motion

--- src/components/Droppable.jsx
+++ src/components/Droppable.jsx
@@ -1,4 +1,5 @@
 import { useDroppable } from '@dnd-kit/core';
+import { motion } from 'framer-motion';
 
 function Droppable(props) {
     const { isOver, setNodeRef } = useDroppable({
@@ -14,8 +15,27 @@ function Droppable(props) {
     const curr = props.curr ? 'bg-yellow-300' : 'bg-yellow-600';
 
     return (
-        <img src={props.src} alt={props.alt} ref={setNodeRef} className={`m-3 rounded-full flex h-36 w-36 ${curr} ${color} text-center`}>
-        </img>
+        <motion.img 
+        initial={{
+            y:0,
+            scale: 1
+        }}
+        animate={{
+            y:[0,-7,0,7,0],
+            scale: [1, 0.9, 0.85, 0.9, 1]
+        }}
+        exit={{
+            y:0,
+            scale:1
+        }}
+        transition={{
+            duration: 2,
+            times: [0, 0.25, 0.5, 0.75, 1],
+            ease: "linear",
+            repeat: Infinity
+        }}
+        src={props.src} alt={props.alt} ref={setNodeRef} className={`m-3 rounded-full flex h-36 w-36 ${curr} ${color} text-center`}>
+        </motion.img>
     );
 }
 
